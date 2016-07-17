e7b173d2f19c283e458b95fbf703fc62228410b0
scott <scott@SSS-DELL-M6500>
2016-07-17 19:35:38 +0100

    Draft

---
+++ vs2012_vector_optimization_bug/vs2012_vector_optimization_bug/vs2012_vector_optimization_bug.cpp
@@ -0,0 +1,11 @@
+// vs2012_vector_optimization_bug.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
